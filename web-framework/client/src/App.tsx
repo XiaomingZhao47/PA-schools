33d0af5f3ef9238f8d06a23a23e29ad55d9e71f4
zhaox-Users <zhaox@lafayette.edu>
2024-09-10 11:12:46 -0400

    modified title of app

--- web-framework/client/src/App.tsx
+++ web-framework/client/src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>SQLite + React (TypeScript) CRUD App</h1>
+      <h1>PA schools Searcher</h1>
       <AddDataForm />
       <DisplayData />
     </div>
