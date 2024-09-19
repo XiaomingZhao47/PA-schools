b1a2c81afd3d2a3d6ee3c1a0703b4bc0d8cb042c
zhaox-Users <zhaox@lafayette.edu>
2024-09-18 20:58:30 -0400

    add the interface for school

---
+++ web-framework/client/src/types.ts
@@ -0,0 +1,10 @@
+// src/types.ts
+export interface School {
+    id: number;
+    DistrictName: string;
+    Name: string;
+    AUN: string;
+    Schl: string;
+    DataElement: string;
+    DisplayValue: number;
+}
